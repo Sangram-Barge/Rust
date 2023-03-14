a1aee4828f3f0213073cde1e2a98ff532388e491
Sangram Barge <sangrambarge098@gmail.com>
2023-03-14 21:10:28 +0530

    basic text formatting

--- 02_text_formatting/text.rs
+++ 02_text_formatting/text.rs
@@ -1,3 +1,24 @@
 fn main() -> () {
-  println!("hello");
+  // println!("this is simple {}", "text");
+
+  // println!("{1}, {0}, {0}", "sangram", "anushree");
+
+  // println!("{web_dev}, {data_dev}", web_dev="sangram is FSD", data_dev="anu is BDD");
+
+  // println!("number formatting");
+  // println!("decimal {}", 23876);
+  // println!("binary {:b}", 23876);
+  // println!("octal {:o}", 23876);
+  // println!("hex {:x}", 23876);
+  // println!("big hex {:X}", 23876);
+
+  // println!("________________________________");
+
+  // println!("{number:>5}", number=1);
+  // println!("{number:0<5}", number=1);
+
+  let number: f64 = 1.0;
+  let width: usize = 5;
+  println!("{number:>width$}");
+
 }
---
+++ 02_text_formatting/todo.rs
@@ -0,0 +1,5 @@
+fn main() {
+  let pi = 3.141592;
+  println!("value of pi is {}", pi);
+  println!("value of pi formatted is {:.3}", pi);
+}
